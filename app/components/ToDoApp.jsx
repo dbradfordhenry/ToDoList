0775d0016f1e64e23108ecd3ec990278f9d24094
Derrick Henry <dbradfordhenry@gmail.com>
2017-01-15 14:39:18 -0500

    adds static array of items

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+  getInitialState: function (){
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Wash the dishes'
+        },
+        {
+          id: 3,
+          text: 'Get chicken'
+        },
+        {
+          id: 4,
+          text: 'Fold the laundry'
+        }
+      ]
+
+    };
+  },
 render: function () {
+  var {todos} = this.state;
 return (
   <div>
-    ToDoApp.jsx
+    <ToDoList todos={todos}/>
   </div>
 )
 }
---
+++ app/components/ToDoList.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var ToDo = require('ToDo');
+
+var ToDoList = React.createClass({
+
+  render: function (){
+    var {todos} = this.props;
+
+    var rendorToDos = () => {
+      return todos.map((todo) => {
+        return (
+          <ToDo key={todo.id}{...todo}/>
+        );
+      });
+    };
+    return (
+      <div>
+      {rendorToDos()}
+      </div>
+    )
+
+  }
+});
+module.exports = ToDoList;
