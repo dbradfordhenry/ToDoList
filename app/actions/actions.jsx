1e824a32cd3497ce2eabfb12cfea5afbe2d8769b
Derrick Henry <Derrick@Derricks-iMac.home>
2017-02-06 21:44:51 -0500

    add actions and action tests

---
+++ app/actions/actions.jsx
@@ -0,0 +1,25 @@
+export var setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  };
+};
+export var toggleToDo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED',
+  };
+};
+export var addToDo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  };
+
+
+};
---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,41 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+    it('should generate search text action', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'Some search text'
+      };
+      var res = actions.setSearchText(action.searchText);
+
+      expect(res).toEqual(action);
+    });
+  it('should generate add toggleShowCompleted action', () => {
+
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var res = actions.toggleShowCompleted();
+    expect(res).toEqual(action);
+  });
+
+    it('should generate add todo action', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'thing to do'
+      };
+      var res = actions.addToDo(action.text);
+
+      expect(res).toEqual(action);
+    });
+
+    it('should generate toggleTodo action', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+      var res = actions.toggleToDo(action.id);
+      expect(res).toEqual(action);
+    })
+  });
